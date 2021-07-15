206c84d920c8b1140a4bc84647ca39ef49787fa2
Zhiwei Huang <3190103047@zju.edu.cn>
2021-07-15 14:44:50 +0800

    chapter4

---
+++ Homework-Chapter-4/Homework/app/src/main/java/com/bytedance/camp/chapter4/MainActivity.java
@@ -0,0 +1,18 @@
+package com.bytedance.camp.chapter4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.SeekBar;
+
+import com.bytedance.camp.chapter4.widget.Clock;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
